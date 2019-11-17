f9a2e79c5a20fbc05c439f5853f29286434de7ca
chachy <shalomadecoolboy@outlook.com>
2019-11-17 18:29:20 +0000

    Add HttpClient

--- src/main/java/net/chachy/particlemod/utils/ChachyMod.java
+++ src/main/java/net/chachy/particlemod/utils/ChachyMod.java
@@ -1,12 +1,12 @@
 package net.chachy.particlemod.utils;
 
 import com.google.gson.JsonParser;
-import net.chachy.particlemod.utils.http.HttpUtils;
+import net.chachy.httpclient.request.Request;
+import net.chachy.httpclient.request.RequestBuilder;
+import net.chachy.httpclient.request.type.MethodType;
+import net.chachy.particlemod.ParticleMod;
 import net.chachy.particlemod.utils.utils.DevUtils;
 import net.chachy.particlemod.utils.utils.Mod;
-import net.chachy.particlemod.ParticleMod;
-
-import java.io.IOException;
 
 public class ChachyMod {
     private String API_URL = "https://api.chachy.co.uk";
@@ -24,27 +24,30 @@ public class ChachyMod {
 
 
     public final String getVersion(Mod mod) {
+        final Request request = new RequestBuilder("https://api.chachy.co.uk/get/mod/" + mod.getModName(), MethodType.GET).build();
         try {
-            return new JsonParser().parse(HttpUtils.INSTANCE.get("https://api.chachy.co.uk/get/mod/" + mod.getModName())).getAsJsonObject().get("version").getAsString();
-        } catch (IOException e) {
+            return new JsonParser().parse(request.getGETResponse()).getAsJsonObject().get("version").getAsString();
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return ParticleMod.INSTANCE.VERSION;
     }
 
     public final String getVersion(String endpoint, String mod) {
+        final Request request = new RequestBuilder(API_URL + endpoint + mod, MethodType.GET).build();
         try {
-            return new JsonParser().parse(HttpUtils.INSTANCE.get(API_URL + endpoint + mod)).getAsJsonObject().get("version").getAsString();
-        } catch (IOException e) {
+            return new JsonParser().parse(request.getGETResponse()).getAsJsonObject().get("version").getAsString();
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return ParticleMod.INSTANCE.VERSION;
     }
 
     private String getVersion(String mod) {
+        final Request request = new RequestBuilder("https://api.chachy.co.uk/get/mod/" + mod, MethodType.GET).build();
         try {
-            return new JsonParser().parse(HttpUtils.INSTANCE.get("https://api.chachy.co.uk/get/mod/" + mod)).getAsJsonObject().get("version").getAsString();
-        } catch (IOException e) {
+            return new JsonParser().parse(request.getGETResponse()).getAsJsonObject().get("version").getAsString();
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return ParticleMod.INSTANCE.VERSION;
